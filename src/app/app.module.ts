f1631a1a398f5fc04655327db8fa07a08754e5ad
Najib EL BERKAOUI <elberkaoui@SPIRIT.LOCAL>
2018-11-02 12:26:16 +0100

    Face detect

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FaceDetectComponent } from './face-detect/face-detect.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FaceDetectComponent
   ],
   imports: [
     BrowserModule,
